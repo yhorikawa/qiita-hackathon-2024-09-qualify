8aab18463db13e7c4b76b8e1b4b260bca94305b4
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-09-17 14:06:04 +0000

    🔧 post create commandでgit pullするように

--- .devcontainer/postCreatecommand.sh
+++ .devcontainer/postCreatecommand.sh
@@ -3,6 +3,12 @@
 # General Settings
 pnpm config set store-dir /home/node/.local/share/pnpm/store
 
+
+## git
+git config --global --add safe.directory /workspaces/qiita-hackathon-2024-09-qualify
+git config --global pull.rebase false
+git pull
+
 ## Install dependencies
 pnpm install
 
