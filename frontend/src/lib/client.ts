a14248d6a7b24dbc2843f5a8ba092415e054a8b5
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-09-18 23:33:22 +0000

    fix lint

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,5 +1,5 @@
 import { hc } from "hono/client";
-import { AppType } from "../../../backend/src/index";
+import type { AppType } from "../../../backend/src/index";
 
 const ENDPOINT =
   process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3000";
