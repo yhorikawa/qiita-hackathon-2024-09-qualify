10b13ea340ddb7098f2559af4f7704573958cc08
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-09-21 09:41:26 +0000

    ✨ Add seed data for conversations and messages

--- backend/db/query.sql
+++ backend/db/query.sql
@@ -20,6 +20,16 @@ WHERE
 -- name: createMessage :exec
 INSERT INTO Messages (conversation_id, sender, message) VALUES (@conversation_id, @sender, @message);
 
+-- name: getMessagesByConversationId :many
+SELECT
+    *
+FROM
+    Messages
+WHERE
+    conversation_id = @conversation_id
+ORDER BY
+    created_at DESC;
+
 -- name: getConversationMessages :many
 SELECT
     *
--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS Conversations (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-  code TEXT NOT NULL,
-	created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
-	updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  code TEXT NOT NULL UNIQUE,
+  created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+  updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
 
 CREATE TABLE IF NOT EXISTS Messages (
- 	id INTEGER PRIMARY KEY AUTOINCREMENT,
- 	conversation_id INTEGER NOT NULL,
- 	sender TEXT NOT NULL,
- 	message TEXT NOT NULL,
- 	created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
- 	updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
- 	FOREIGN KEY (conversation_id) REFERENCES Conversations(id)
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  conversation_id INTEGER NOT NULL,
+  sender TEXT NOT NULL,
+  message TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+  updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+  FOREIGN KEY (conversation_id) REFERENCES Conversations(id)
 );
 
 CREATE TABLE IF NOT EXISTS Documents (
