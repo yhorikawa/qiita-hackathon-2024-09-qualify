57950d26a95d5c82c497be425c8641120b24a43b
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-09-21 12:22:42 +0000

    Add ChatInput component and related files

---
+++ frontend/src/app/chat/[id]/_components/ChatInput/ChatInput.tsx
@@ -0,0 +1,10 @@
+import type { FC } from "react";
+
+export const ChatInput: FC = () => {
+  return (
+    <>
+      <input type="text" placeholder="ぷれーすほるだぁ" />
+      <button type="button">✈</button>
+    </>
+  )
+}
