35ce1b8ab51aef57461e5651ccc622ff85bcda8e
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-09-21 13:16:13 +0000

    ChatInput

--- frontend/src/app/chat/[id]/_components/ChatInput/ChatInput.tsx
+++ frontend/src/app/chat/[id]/_components/ChatInput/ChatInput.tsx
@@ -1,10 +1,13 @@
+'use client';
 import type { FC } from "react";
+import { usePostChat } from "./use-post-chat";
 
 export const ChatInput: FC = () => {
+  const { text, setText, handleAction, isLoading } = usePostChat();
   return (
     <>
-      <input type="text" placeholder="ぷれーすほるだぁ" />
-      <button type="button">✈</button>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="ぷれーすほるだぁ" />
+      <button onClick={handleAction} disabled={isLoading} type="button">✈</button>
     </>
   );
 };
