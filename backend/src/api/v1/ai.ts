2a64d24bf5eca3a0703cff5045522e4578fa8d48
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-09-21 08:04:26 +0000

    🚨 fix lint

--- backend/src/api/v1/ai.ts
+++ backend/src/api/v1/ai.ts
@@ -6,12 +6,16 @@ import type { Bindings } from "./index";
 const app = new Hono<{ Bindings: Bindings }>();
 app.post(
   "/conversations/start",
-  zValidator("json", z.object({ message: z.string() })), async (c) => {
+  zValidator("json", z.object({ message: z.string() })),
+  async (c) => {
     const { message } = await c.req.valid("json");
 
     c.status(201);
-    return c.json({ success: true, data: {conversation_id: 1234, ai_response: message} });
-  }
+    return c.json({
+      success: true,
+      data: { conversation_id: 1234, ai_response: message },
+    });
+  },
 );
 
 export default app;
