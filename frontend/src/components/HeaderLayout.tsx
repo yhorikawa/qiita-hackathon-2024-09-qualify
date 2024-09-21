38c43927980385e96b7966d775e0be2e56faed18
hikari0709 <tanimoto0709@gmail.com>
2024-09-21 13:26:49 +0000

    create DropdownMenu

--- frontend/src/components/HeaderLayout.tsx
+++ frontend/src/components/HeaderLayout.tsx
@@ -1,17 +1,45 @@
+"use client";
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
 
 export const HeaderLayout = () => {
+  const path = usePathname();
+
   return (
-    <header className="bg-slate-200 p-3 flex justify-between mb-3">
+    <header className="bg-white py-2 px-20 flex justify-between items-center">
       <p className="font-semibold text-sm leading-5">
         なんかサービスロゴ的なもの
       </p>
-      <Link
-        href="/chat"
-        className="font-semibold text-sm leading-5 bg-black text-white p-2"
-      >
-        AIと壁打ち作成
-      </Link>
+      {path === "/" && (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="bg-violet-800 text-white py-2 px-4 rounded-md text-lg font-bold leading-relaxed focus:outline-offset-[3px] hover:bg-violet-600">
+            記事を作成する
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="flex gap-2">
+              <Link href="/new" className="text-base leading-relaxed">
+                新規作成
+              </Link>
+              <ChevronRight className="ml-auto h-6 w-6 opacity-60" />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="flex gap-2">
+              <Link href="/chat" className="text-base leading-relaxed">
+                AIと壁打ち作成
+              </Link>
+              <ChevronRight className="ml-auto h-6 w-6 opacity-60" />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </header>
   );
 };
