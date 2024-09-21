5132b7feaa87fa6ddf261d8d1e292104973ac41d
hikari0709 <tanimoto0709@gmail.com>
2024-09-21 11:30:34 +0000

    create ArticleCard component

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      opacity: {
+        "87": ".87",
+      },
     },
   },
   plugins: [require("preline/plugin")],
