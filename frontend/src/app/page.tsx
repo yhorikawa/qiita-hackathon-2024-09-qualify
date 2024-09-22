0dfd446f5b7eed9941048826a0a1f369d45fbc34
miyazaki.kaito <50905693+kaitat@users.noreply.github.com>
2024-09-22 02:58:04 +0000

    fix lint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,26 +3,25 @@ import Link from "next/link";
 import { client } from "#/lib/client";
 
 const Page: NextPage = async () => {
-  const res = await client.api.v1.documents.$get()
+  const res = await client.api.v1.documents.$get();
   if (!res.ok) return null;
 
-  const { data: { documents } } = await res.json();
+  const {
+    data: { documents },
+  } = await res.json();
 
   return (
     <>
-      {
-        documents.map((document) => (
-          <Link href={`/articles/${document.id}`}
-            key={document.id}
-            className="w-full p-3 bg-white m-2 font-bold block"
-          >
-            <h2>
-              {document.content.substring(0, 15)}...
-            </h2 >
-            <p>{document.createdAt}</p>
-          </Link >
-        ))
-      }
+      {documents.map((document) => (
+        <Link
+          href={`/articles/${document.id}`}
+          key={document.id}
+          className="w-full p-3 bg-white m-2 font-bold block"
+        >
+          <h2>{document.content.substring(0, 15)}...</h2>
+          <p>{document.createdAt}</p>
+        </Link>
+      ))}
     </>
   );
 };
